689fa89df0d96b085253555e0e4181d7adc1526b
jadhav-dipali <jadhavdipali924@gmail.com>
2023-03-19 13:36:03 +0530

    script the my file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let rect = document.getElementById("rect");
 
 let btn = document.getElementById("btn");
-
+ 
 btn.addEventListener("click" ,()=>{
     rect.style.backgroundColor="red";
+    rect.style.border="2px solid pink"
 })
+
